4a3082c6a112fc1f154b0ce614ac19f3d92b2ef1
Dazza Greenwood <dazza@civics.com>
2016-08-27 04:48:41 -0400

    wide assortment

---
+++ ToScan/0_0_0_MODEL-EntireEnterpriseEncapsulation/EncapsulatedEntireEnterprise.mm
@@ -0,0 +1,25 @@
+﻿<map version="0.9.0">
+    <node TEXT=" EncapsulatedEntireEnterprise">
+        <node TEXT="Existing Products
+and Services">
+            <node TEXT="AutoMate™ "></node>
+            <node TEXT="Docker"></node>
+        </node>
+        <node TEXT="Standards">
+            <node TEXT="OMG-BPM-Plus"></node>
+            <node TEXT="XBRL"></node>
+        </node>
+        <node TEXT="Law and Policy">
+            <node TEXT="GPEA"></node>
+            <node TEXT="SEC-XBRL"></node>
+        </node>
+        <node TEXT="Allies and Advocates">
+            <node TEXT="DAOhub?"></node>
+            <node TEXT="Tim Hwang?"></node>
+        </node>
+        <node TEXT="Automated Credit Union">
+            <node TEXT="BlockCreditUnion"></node>
+            <node TEXT="Automatrust"></node>
+        </node>
+    </node>
+</map>
